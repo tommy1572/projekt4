4198679783a33d570f4cbd03187d8ab86a775fbf
emiliesra <119939432+emiliesra@users.noreply.github.com>
2023-03-27 20:34:40 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+  function openDropdown() {
+    document.getElementsByClassName("myDropdown").classList.toggle("show");
+  }
