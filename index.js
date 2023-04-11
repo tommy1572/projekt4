971bd6c667b76bda7af445ff9e420543d811c081
emiliesra <119939432+emiliesra@users.noreply.github.com>
2023-04-11 10:29:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,11 @@ function openNav() {
   }
 
 /* Menubar web */
-function myfunction() {
-  document.getElementByClassName("menuMyDropdown").classList.toggle("show");
-}
-
-window.onclick = function(e) {
-  if (!e.target.matches('.menudropbtn')) {
-  var myDropdown = document.getElementByClassName("menuMyDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
+function openMenuDropdown() {
+  let dropdownContent = document.querySelector(".menudropdown-content");
+  if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+  } else{
+      dropdownContent.style.display = "none";
   }
 }
