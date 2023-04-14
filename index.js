03d36d10af9084cc7e2816e22af44c68e2967c51
JohannaLanggaard <johanna.langgaard@gmail.com>
2023-04-14 13:17:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+//kontakt form starter her
+
+  select_element = document.getElementById("semester");
+  let sprog = ["1. semester", "2. semester", "3. semester", "4. semester", "5. semester", "6. semester"];
+
+
+  // Udvider option-boks baseret på antal sprog i array'et
+  for (let i=0; i < sprog.length; i++) {
+  let opt = sprog[i];
+  select_element.innerHTML += "<option value=\" "+ opt +"\">"+ opt + "</option";
+  }
+
+  document.getElementById("semester").onchange = function(){
+  if (document.getElementById("semester").value == ' semester')
+  location.replace('index.html');
+  else if (document.getElementById("semester").value == ' semester')
+  location.replace('indexEn.html');
+  else
+  location.replace('indexDe.html')
+  }
+  document.getElementById("semester").selectedIndex = 1;
+
+//kontakt form slutter her
+
   /* Burgermenu */
   function openNav() {
     document.getElementById("myNav").style.height = "100%";
@@ -81,27 +105,3 @@ function showSlides(n) {
   }
 
 // karrusel slut
-
-//kontakt form starter her
-
-  select_element = document.getElementById("semester");
-  let sprog = ["1. semester", "2. semester", "3. semester", "4. semester", "5. semester", "6. semester"];
-
-
-  // Udvider option-boks baseret på antal sprog i array'et
-  for (let i=0; i < sprog.length; i++) {
-  let opt = sprog[i];
-  select_element.innerHTML += "<option value=\" "+ opt +"\">"+ opt + "</option";
-  }
-
-  document.getElementById("semester").onchange = function(){
-  if (document.getElementById("semester").value == ' semester')
-  location.replace('index.html');
-  else if (document.getElementById("semester").value == ' semester')
-  location.replace('indexEn.html');
-  else
-  location.replace('indexDe.html')
-  }
-  document.getElementById("semester").selectedIndex = 1;
-
-//kontakt form slutter her
