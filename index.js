f24e58c9352acd866b4dd115bc5308b933170124
emiliesra <119939432+emiliesra@users.noreply.github.com>
2023-04-18 20:24:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,62 +26,6 @@
     }
   }
 
-  //timer starter her
-
-var timers = [
-  [new Date("April 30, 2023 15:37:25").getTime(), 'Timer1'],
-  [new Date("May 31, 2023 15:37:25").getTime(), 'Timer2'],
-  [new Date("April 30, 2023 15:37:25").getTime(), 'Timer3'],
-  [new Date("May 31, 2023 15:37:25").getTime(), 'Timer4']
-  ];
-var x = setInterval(function () {
-  timers.forEach(function (element) {
-      var now = new Date().getTime();
-      var distance = element[0] - now;
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      document.getElementById(element[1]).innerHTML = ' <p id="days" class="float_left">' + days + '<br><span>DAGE</span></p> <p id="hours" class="float_left">' + hours + '<br><span>TIMER</span></p> <p id="minutes" class="float_left">' + minutes + '<br><span>MINUTTER</span></p> <p id="seconds" class="float_left">' + seconds + '<br><span>SEKUNDER</span></p>';
-      if (distance < 0) {
-          clearInterval(x);
-          document.getElementById(element[1]).innerHTML = "EXPIRED";
-      }
-  });
-});
-
-//timer slutter her
-
-// pop-up starter
-const popOutButton = document.getElementById("button"); 
-const bg_container = document.querySelector("bgpopContainer");
-const exitPopUp = document.getElementById("bg_container");
-
-popOutButton.addEventListener("click", popOutNow);
-document.addEventListener("mouseout", exitPage);
-
-function popOutNow(e) {
-    e.preventDefault();
-    document.querySelector(".bgpopContainer").style.display = "flex";
-}
-
-const cancelPop = document.getElementById("close");
-
-cancelPop.addEventListener("click", cancelPopOut)
-
-function cancelPopOut(e) {
-    e.preventDefault();
-    document.querySelector(".bgpopContainer").style.display = "none";
-}
-
-function exitPage(event) {
-    if (event.clientY < 50) {
-        document.removeEventListener("mouseout", exitPage);
-        exitPopUp.style.display = "flex";
-    }
-}
-// pop-up slut
-
   // Galleri
 let slideShow = 0;
 kørSlide();
@@ -137,6 +81,62 @@ function showSlides(n) {
   }
 
 // karrusel slut
+
+// pop-up starter
+const popOutButton = document.getElementById("button"); 
+const bg_container = document.querySelector("bgpopContainer");
+const exitPopUp = document.getElementById("bg_container");
+
+popOutButton.addEventListener("click", popOutNow);
+document.addEventListener("mouseout", exitPage);
+
+function popOutNow(e) {
+    e.preventDefault();
+    document.querySelector(".bgpopContainer").style.display = "flex";
+}
+
+const cancelPop = document.getElementById("close");
+
+cancelPop.addEventListener("click", cancelPopOut)
+
+function cancelPopOut(e) {
+    e.preventDefault();
+    document.querySelector(".bgpopContainer").style.display = "none";
+}
+
+function exitPage(event) {
+    if (event.clientY < 50) {
+        document.removeEventListener("mouseout", exitPage);
+        exitPopUp.style.display = "flex";
+    }
+}
+// pop-up slut
+
+  //timer starter her
+
+  var timers = [
+    [new Date("April 30, 2023 15:37:25").getTime(), 'Timer1'],
+    [new Date("May 31, 2023 15:37:25").getTime(), 'Timer2'],
+    [new Date("April 30, 2023 15:37:25").getTime(), 'Timer3'],
+    [new Date("May 31, 2023 15:37:25").getTime(), 'Timer4']
+    ];
+  var x = setInterval(function () {
+    timers.forEach(function (element) {
+        var now = new Date().getTime();
+        var distance = element[0] - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById(element[1]).innerHTML = ' <p id="days" class="float_left">' + days + '<br><span>DAGE</span></p> <p id="hours" class="float_left">' + hours + '<br><span>TIMER</span></p> <p id="minutes" class="float_left">' + minutes + '<br><span>MINUTTER</span></p> <p id="seconds" class="float_left">' + seconds + '<br><span>SEKUNDER</span></p>';
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById(element[1]).innerHTML = "EXPIRED";
+        }
+    });
+  });
+  
+  //timer slutter her
 
 //kontakt form starter her
 
