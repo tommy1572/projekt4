5e34d22b36d27c7bd07e0d0251bfe41f7c8e2f12
emiliesra <119939432+emiliesra@users.noreply.github.com>
2023-04-20 08:41:33 +0200

    Create læsmere.js

---
+++ læsmere.js
@@ -0,0 +1,57 @@
+/* Burgermenu (Emilie)*/
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  
+  function openDropdown() {
+    let dropdownContent = document.querySelector(".dropdown-content");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else{
+        dropdownContent.style.display = "none";
+    }
+  }
+  
+  /* Menubar web (Emilie) */
+  function openMenuDropdown() {
+    let dropdownContent = document.querySelector(".menudropdown-content");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else{
+        dropdownContent.style.display = "none";
+    }
+  }
+
+  /*hjerte START*/
+function increaseCount() {
+    var countElement = document.getElementById("count");
+    var count = parseInt(countElement.innerText);
+    countElement.innerText = count + 1;
+    document.querySelector('.heart-btn').style.color = 'red';
+  }
+  /*hjerte SLUT*/
+  
+  /*læs mere karrusel START */
+  var billedeIndex = 1;
+  visKarrusel(billedeIndex);
+  
+  function plusKarrusel(n) {
+    visKarrusel(billedeIndex += n);
+  }
+  
+  function visKarrusel(n) {
+    var i;
+    var karrusel = document.getElementsByClassName("minBilleder");
+    if (n > karrusel.length) {billedeIndex = 1}
+    if (n < 1) {billedeIndex = karrusel.length}
+    for (i = 0; i < karrusel.length; i++) {
+      karrusel[i].style.display = "none";
+    }
+    karrusel[billedeIndex-1].style.display = "block";
+  }
+  
+  /*læs mere karrusel SLUT */
